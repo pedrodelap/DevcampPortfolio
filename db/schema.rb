bab6ec73889d95813c325880fe1cf57e78f1ad71
pedro delgado aparicio <pedro.delgado.aparicio@gmail.com>
2018-08-31 01:55:07 -0500

    Se agregaron elementos de Portafolio a través del Resource generator

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_30_193928) do
+ActiveRecord::Schema.define(version: 2018_08_31_064013) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "portafolios", force: :cascade do |t|
+    t.string "title"
+    t.string "subtitle"
+    t.text "body"
+    t.text "main_image"
+    t.text "thumb_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
