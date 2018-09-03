535f37c8979171753c9ae9a70f60c0e273f3194e
pedro delgado aparicio <pedro.delgado.aparicio@gmail.com>
2018-09-02 23:27:13 -0500

    Configurar rutas personalizadas para páginas

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :portafolios
   resources :blogs
   get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+  get 'about-me', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'pages#home'
 end
