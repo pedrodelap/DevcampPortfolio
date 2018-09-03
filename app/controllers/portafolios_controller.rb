cabaeb0117f7826e1e1b4345ddac91a1f8d59118
pedro delgado aparicio <pedro.delgado.aparicio@gmail.com>
2018-09-02 22:01:29 -0500

    Funcionalidad de edición y actualización implementada

--- app/controllers/portafolios_controller.rb
+++ app/controllers/portafolios_controller.rb
@@ -1,7 +1,42 @@
 class PortafoliosController < ApplicationController
 
   def index
-  @portfolio_items = Portafolio.all
+    @portfolio_items = Portafolio.all
   end
+
+  def new
+    @portfolio_item = Portafolio.new
+  end
+
+  def create
+    @portfolio_item = Portafolio.new(params.require(:portafolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portafolios_path, notice: 'Your portfolio item is now live.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+
+  def edit
+    @portfolio_item = Portafolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portafolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+
 
 end
